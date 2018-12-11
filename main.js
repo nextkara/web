b06a22926a0fb47a3f7d579c4419bb36a9b3927b
nextkara <nextkara@hotmail.com>
2018-12-11 16:33:21 +0800

    inside development

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
-$("#hide").click(function () {
-    $("left_side").hide();
-});
+// $("#hide").click(function () {
+//     $("left_side").hide();
+// });
 
-$("#show").click(function () {
-    $("left_side").show();
-});
+// $("#show").click(function () {
+//     $("left_side").show();
+// });
+
+
+// $(document).ready(function (){
+//     $("#center_button_show").hide();
+//     $("#center_button").click(function (){
+//         $("#left_side").hide(1000);
+//         $("#center_button").hide();
+//         $("#center_button_show").show();
+//     });
+//     $("#center_button_show").click(function (){
+//         $("#left_side").show(500);
+//         $("#center_button_show").hide();
+//         $("#center_button").show();
+//     });
+// });
